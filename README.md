7cf75efcc0d1a9b4cf94febe5f3481782ec8b0ba
mstpierre73 <naranjaqc@hotmail.com>
2019-05-27 20:18:19 +0000

    add posts file

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # nodeWithColt
-Learning Node, Express and EJS with Colt Steele
+Learning Node, Express and EJS with Colt Steele, using goormIDE
--- introEJS/app.js
+++ introEJS/app.js
@@ -14,6 +14,17 @@ app.get("/love/:puppies", (req, res) => {
 	res.render("love.ejs", {puppies: puppies});
 });
 
+//Defining a dynamic route 
+app.get("/posts", (req, res) => {
+	let posts = [
+		{title: "Meet my new baby", author: "Susy"},
+		{title: "I don't like dogs", author: "Math"},
+		{title: "Who made this pie ?", author: "Bianca"}
+	];
+	
+	res.render("posts.ejs", {posts: posts});
+});
+
 //Starting the server on port 3000
 app.listen(port, function(){
 	console.log("The app is running on port 3000");
