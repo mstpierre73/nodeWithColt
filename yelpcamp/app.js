92208b6e92a3e2f75e237e8957739513ba0461dc
mstpierre73 <naranjaqc@hotmail.com>
2019-06-05 14:27:21 +0000

    connect mongoose with yelpcamp project

--- yelpcamp/app.js
+++ yelpcamp/app.js
@@ -2,16 +2,9 @@
 const express = require("express");
 const request = require("request");
 const bodyParser =require("body-parser");
+const mongoose = require("mongoose");
 const PORT = 3000;
 const app = express();
-
-
-
-const campgrounds = [
-	{name: "Réserve faunique de Port-Cartier-Sept-îles", image: "http://www.villeport-cartier.com/upload/villeport-cartier/editor/image/Chute%20MacDonald_JB%C3%A9langer%20(10).JPG" },
-	{name: "Parc nationa d'Anticosti", image: "https://www.sepaq.com/resources/images/pq/pan/caroussel/ant_001.jpg" },
-	{name: "Parc national de l'île-Bonnaventure-et-du-Rocher-Percé", image: "https://www.geoparcdeperce.com/wp-content/uploads/2019/05/geoparc_roche_perce_bonjour_gaspesie.jpg" },
-];
 
 //Define engine template for ejs
 app.set("view engine", "ejs");
@@ -19,24 +12,61 @@ app.set("view engine", "ejs");
 //Setting bodyParser
 app.use(bodyParser.urlencoded({extended: true}));
 
+//connect the Database
+mongoose.connect("mongodb://localhost:27017/yelpcamp", {useNewUrlParser: true});
+
+//db schema setup
+const campgroundSchema = new mongoose.Schema({
+	name: String,
+	image: String
+});
+
+const Campground = mongoose.model("Campground", campgroundSchema);
+
+/*Campground.create({
+	name: "Réserve faunique de Port-Daniel",
+	image: "https://www.quebecoriginal.com/fiche/images/800x600/9c2d51c1-d5bc-47b6-9fdc-cde1157b5dcd/reserve-faunique-de-port-daniel-chalet-lac-de-lile.jpg"
+}, function (err, campground){
+	if(err){
+		console.log(err);
+	} else {
+		console.log("new campground added to db");
+		console.log(campground);
+	}
+});*/
+
+
 //Define home page route
 app.get("/", (req, res) => {
 	res.render("index");
 });
 
-//Define get campings route
+//Define get campings route and get all campground from db
 app.get("/campings", (req, res) => {
-	res.render("campings", {campgrounds: campgrounds});
+	Campground.find({}, function(err, allCamps){
+		if(err){
+			console.log("Houston, we've got a problem!");
+			console.log("Error : " + err);
+		} else {
+			res.render("campings", {campgrounds: allCamps});
+		}
+	});
 });
 
 
-//Define post campings route
+//Define post campings route and add new campground to db
 app.post("/campings", (req, res) => {
 	let newCamp = req.body.newCamp;
 	let imgURL = req.body.imgURL;
 	let newCampObj = {name: newCamp, image: imgURL};
-	campgrounds.push(newCampObj);
-	res.redirect("/campings");
+	Campground.create(newCampObj, function(err, newCamp){
+		if(err){
+			console.log("Cannot add this campground to db");
+			console.log("Error : " + err);
+		} else {
+			res.redirect("/campings");
+		}
+	});
 });
 
 //Define add new campground route
