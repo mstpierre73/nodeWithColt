34821ead7a82ae5757eb195932d3af35994810e2
mstpierre73 <naranjaqc@hotmail.com>
2019-06-13 14:48:53 +0000

    finish auth process in secret page app

--- blog/auth/app.js
+++ blog/auth/app.js
@@ -14,6 +14,7 @@ mongoose.connect('mongodb://localhost/secret', {useNewUrlParser: true});
 mongoose.set('useFindAndModify', false);
 const app = express();
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(require('express-session')({
 	secret: "It is hard to learn programming but I love it",
 	resave: false,
@@ -22,6 +23,7 @@ app.use(require('express-session')({
 app.use(passport.initialize());
 app.use(passport.session());
 
+passport.use(new localStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
@@ -33,9 +35,56 @@ app.get("/", (req, res) =>{
 });
 
 //Secret route
-app.get("/secret", (req, res) =>{
+app.get("/secret", isLoggedIn, (req, res) =>{
 	res.render("secret");
 });
+
+//Auth signUp route - Show sign up form
+app.get("/register", (req, res) =>{
+	res.render("register");	
+}); 
+
+//Auth signUp route - send data from sign up form
+app.post("/register", (req, res) =>{
+	User.register(new User({username: req.body.username}), req.body.password, (err, user)=>{
+		if(err){
+			console.log(err);
+			res.render('register');
+		} else {
+			passport.authenticate('local')(req, res, ()=>{
+				res.redirect("/secret");
+			});
+		}
+	});
+});
+
+//Auth login route - show login form
+app.get("/login", (req, res)=>{
+	res.render("login");
+});
+
+//Auth login route - send data from login form to passport middleware
+app.post("/login", passport.authenticate('local', 
+	{successRedirect: "/secret",
+	failureRedirect: "/login"}),
+	(req, res)=>{});
+
+//Auth logout
+app.get("/logout", (req, res)=>{
+	req.logout();
+	res.redirect("/");
+});
+
+//CREATE MIDDLEWARE=====================================================================
+
+//function to check if user is logged in
+function isLoggedIn(req, res, next){
+	if(req.isAuthenticated()){
+		return next();
+	} else {
+		res.redirect("/login");
+	}
+}
 
 
 //Start the server=======================================================================
