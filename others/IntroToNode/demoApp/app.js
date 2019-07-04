26379d4cd7af5cefbeb0786483e3e574ec5b75b7
mstpierre73 <naranjaqc@hotmail.com>
2019-07-04 17:18:00 +0000

    delete others before deploy

--- others/IntroToNode/demoApp/app.js
+++
@@ -1,5 +0,0 @@
-var cat = require("cat-me");
-var jokes = require("knock-knock-jokes");
-
-console.log(cat());
-console.log(jokes());
--- others/IntroToNode/echo.js
+++
@@ -1,8 +0,0 @@
-function echo(script, num){
-	for (var i= 0; i < num; i++){
-		console.log(script);
-	}
-}
-
-echo("Echo!!!", 10);
-echo("Tater Tots", 3);
